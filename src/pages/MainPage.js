951378db51b9771e33998bb561f7fbec445432d1
khj <soswoxm93@gmail.com>
2025-03-07 20:37:08 +0900

    일단 컨테이너 틀 잡아놨음.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,12 +2,27 @@ import React , {useState} from 'react';
 import './MainPage.css';
 
 const MainPage = () => {
-  return (
-    <div className="Main-Page">
-
-        
-    </div>
-  )
+    return (
+        <div className="main-Container">
+          <div className="side-Menu-Container">sideMenu</div>
+          <div className="content-Wrapper">
+            <div className="ad-Container">ad-Container</div>
+            <div className="character-Select-Container">character-select-Container</div>
+            <div className="main-Content">
+              <div className="left-Column">
+                <div className="character-Container">character-Container</div>
+                <div className="arkPassive-Container">arkpassive-Container</div>
+                <div className="title-Container">title-Container</div>
+              </div>
+              <div className="right-Column">
+                <div className="search-Container">search-Container</div>
+                <div className="filter-Container">filter-Container</div>
+                <div className="party-Container">party-Container</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
 }
 
 export default MainPage
