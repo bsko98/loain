5203f8ab871f5023695908ce28ac6fa7194cedde
khj <soswoxm93@gmail.com>
2025-03-07 15:42:32 +0900

    .

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React , {useState} from 'react';
+import './MainPage.css';
+
+const MainPage = () => {
+  return (
+    <div className="Main-Page">
+
+        
+    </div>
+  )
+}
+
+export default MainPage
