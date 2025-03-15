aa8b9d2659cbd18059ed89eea5f4ecabd8f0646e
khj <soswoxm93@gmail.com>
2025-03-16 01:44:11 +0900

    dev 고침

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -17,32 +17,33 @@ const MainPage = () => {
     };
 
     return (
-      <div>
-        <div className="main-Container">
-            <div className="content-Wrapper">
-                <div className="ad-Container">ad-Container</div>
-                <div className="character-Select-Container">
-                    <CharacterSelectContainer />
-                </div>
-                <div className="main-Content">
-                    <div className="left-Column">
-                        <div className="character-Container">character-Container</div>
-                        <div className="arkPassive-Container">arkpassive-Container</div>
-                        <div className="card-Container">card-Container</div>
-                        <div className="title-Container">title-Container</div>
+        <div>
+            <div className="main-Container">
+                <div className="content-Wrapper">
+                    <div className="ad-Container">ad-Container</div>
+                    <div className="character-Select-Container">
+                        <CharacterSelectContainer />
                     </div>
-                    <div className="right-Column">
-                        <div className="right-Container">
-                            <div className="right-Top-Container">
-                                <PartyTitleSearchbar setSearchQuery={setSearchQuery} />
-                                <FilterContainer setFilterModalOpen={setFilterModalOpen} selectedFilters={selectedFilters} />
-                            </div>
+                    <div className="main-Content">
+                        <div className="left-Column">
+                            <div className="character-Container">character-Container</div>
+                            <div className="arkPassive-Container">arkpassive-Container</div>
+                            <div className="card-Container">card-Container</div>
+                            <div className="title-Container">title-Container</div>
                         </div>
-                        <div className="party-Container">party-Container</div>
+                        <div className="right-Column">
+                            <div className="right-Container">
+                                <div className="right-Top-Container">
+                                    <PartyTitleSearchbar setSearchQuery={setSearchQuery} />
+                                    <FilterContainer setFilterModalOpen={setFilterModalOpen} selectedFilters={selectedFilters} />
+                                </div>
+                            </div>
+                            <div className="party-Container">party-Container</div>
+                        </div>
                     </div>
                 </div>
+                {isFilterModalOpen && <FilterModal isOpen={isFilterModalOpen} onClose={() => setFilterModalOpen(false)} applyFilters={applyFilters} />}
             </div>
-            {isFilterModalOpen && <FilterModal isOpen={isFilterModalOpen} onClose={() => setFilterModalOpen(false)} applyFilters={applyFilters} />}
         </div>
     );
 };
