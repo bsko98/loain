9f4830355e3952bfefa93ac404db435b47c8aac5
bsko98 <bsko98@naver.com>
2025-03-18 13:21:53 +0900

    chore: 서버 버튼 선택시 해당 캐릭터들만 보이게, 추가하기 버튼에 이미지 추가

--- src/components/myCharacterModal.jsx
+++ src/components/myCharacterModal.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from "react";
 import './myCharacterModal.css'
 import CharacterInfoComponent from './characterInfoComponent'
 import './CharacterChangeModal.css'
+import { ReactComponent as AddButton} from '../assets/images/addButton.svg'
 
 
-const myCharacterModal = ({firstRowButton}) => {
+//TODO - 정보 갱신 누를시 어떤 이벤트가 작동하는지 확인해봐야될듯
+const MyCharacterModal = () => {
 
   const servers = [
     {id:'1', serverName: '루페온'},
@@ -23,28 +25,45 @@ const myCharacterModal = ({firstRowButton}) => {
     {id: '3', characterPhoto:'https://img.lostark.co.kr/armory/3/ADCEB258EB5A436F8E1E3B0F852441B0A85753ABE72E9D29D88C610F885A1120.jpg?v=20250204191040', characterName: '사각나무방패', serverName: '카마인', classInfo:'워로드', itemLevel:'1,592.50'},
   ]  
 
+
+  const [filteredCharacters, setFilteredCharacters] = useState(characters);
+  const [activeServer, setActiveServer] = useState(null);
+  
+  // 서버 필터링 함수
+  const filterByServer = (server) => {
+      if (activeServer === server) {
+          setFilteredCharacters(characters);
+          setActiveServer(null);
+      } else {
+          setFilteredCharacters(characters.filter((char) => char.serverName === server));
+          setActiveServer(server);
+      }
+  };
+
+
+
   return (
     <div className='my-character-container'>
       <div className='my-character-add-button-row'>
           내 캐릭터
           <button className='add-character-buuton'>
-              {/* <AddButton style={{marginTop:'4px'}}/> */}
-              <div style={{width:'49px', height:'17px', fontSize:'11px', marginLeft:'4px', paddingTop:'4.5px'}}>
+              <AddButton style={{marginTop:'4px'}}/>
+              <div style={{width:'49px', height:'17px', fontSize:'11px', marginLeft:'4px', paddingTop:'4.5px'}} onClick={()=>alert("캐릭터 추가 부분이 구현될거임")}>
                   추가하기
               </div>
           </button>
       </div>
       <div className='my-character-server-box'>
-          {servers.map(server => (<button key={server.id} className='character-change-server-button'>{server.serverName}</button>))}
+          {servers.map(server => (<button key={server.id} className='character-change-server-button' onClick={()=>filterByServer(server.serverName)}>{server.serverName}</button>))}
       </div>
       <div className='my-character-info-box'>
-        {characters.map(character=>(
+        {filteredCharacters.map(character=>( 
           <CharacterInfoComponent key={character.characterName} characterPhoto={character.characterPhoto} characterName={character.characterName} 
-          serverName={character.serverName} classInfo={character.classInfo} itemLevel={character.itemLevel}
+          serverName={character.serverName} classInfo={character.classInfo} itemLevel={character.itemLevel} characterId={character.id}
           refreshButton={<div onClick={()=>console.log(character.characterName)} style={{width:'60px', height:'21px', borderRadius:'8px', backgroundColor:'#D28506',color:'white' ,paddingTop:'4px', fontSize:'14px'}}>정보 갱신</div>}/>))}
       </div>
   </div>
   )
 }
 
-export default myCharacterModal
+export default MyCharacterModal
