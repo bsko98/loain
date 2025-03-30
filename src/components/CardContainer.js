6894e9a6bb7db55a49fa612dc0beab93612350b0
ExLightSaber <thunderiker@naver.com>
2025-03-31 01:24:40 +0900

    카드, 타이틀 js 및 css파일 classname 추가 수정 작업

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -4,11 +4,11 @@ import './CardTitleContainer.css';
 const CardContainer = ({ listTitle, listData }) => {
   return (
     <div className='cardtitle-container'>
-      <div className='listtitle'>{listTitle}</div>
+      <div className='list-title'>{listTitle}</div>
       <div className='list-container'>
         {listData.map((data, index) => (
           <div key={index} className='list'>
-            <div className='contenttitle'>{data.Name}({data.AwakenSum}각)</div>
+            <div className='content-title'>{data.Name}({data.AwakenSum}각)</div>
             <div className='content'>{data.EffectType}속성 피해 + {data.EffectSum}%</div>
           </div>
         ))}
--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -4,11 +4,11 @@ import './CardTitleContainer.css';
 const TitleContainer = ({ listTitle, listData }) => {
   return (
     <div className='cardtitle-container'>
-      <div className='listtitle'>{listTitle}</div>
+      <div className='list-title'>{listTitle}</div>
       <div className='list-container'>
         {listData.map((data, index) => (
           <div key={index} className='list'>
-            <div className='contenttitle'>{data.titleName}</div>
+            <div className='content-title'>{data.titleName}</div>
             <div className='content'>{data.titleContent}</div>
           </div>
         ))}
