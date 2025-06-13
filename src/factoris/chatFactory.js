060b34b346eef2c89cd3f65196913024c4c0232a
undoNULL <HS.Kang.Dev@gmail.com>
2025-06-13 18:07:08 +0900

    chore: chat리스트 엔티티, chat팩토리, chat리스트 스테이트 추가

---
+++ src/factoris/chatFactory.js
@@ -0,0 +1,14 @@
+
+export class ChatFactory {
+        static create(
+            timestamp = undefined,
+            senderId = undefined,
+            message = undefined
+    ) {
+        return {
+            timestamp: timestamp,
+            senderId: senderId,
+            message: message
+        }
+    }
+}
