998ec421a452d8ef14db17c88cf476808abf694a
bsko98 <bsko98@naver.com>
2025-08-16 02:09:22 +0900

    feat: 송신부 작성

--- src/components/AddCharacterComponent.jsx
+++ src/components/AddCharacterComponent.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './AddCharacterComponent.css'
 import { ReactComponent as CloseButton} from "../assets/images/CloseButton.svg";
+import { useState } from 'react';
+import { socketManager } from '../socket/socket.js';
 
 const AddCharacterComponent = ({isOpen, onClose}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChangeInputVal =(input)=>{
+        setInputValue(input.target.value);
+    }
+
+    const addCharacter=(inputValue)=>{
+        if(!inputValue){
+            alert("닉네임을 입력해주세요");
+            return;
+        }
+        console.log(inputValue);
+        socketManager.send("addCharacter",{characterName: inputValue});
+    }
+
     if (!isOpen) return null;
   return (
     <div className='filter-modal-overlay'>
@@ -18,10 +36,15 @@ const AddCharacterComponent = ({isOpen, onClose}) => {
                     </div>
                 </div>
                 <div className='add-character-input-area'>
-                    <input type='text' className='add-character-input-area-input' placeholder='캐릭터 닉네임을 입력해주세요.'/>
+                    <input type='text' 
+                        className='add-character-input-area-input' 
+                        placeholder='캐릭터 닉네임을 입력해주세요.' 
+                        value={inputValue}
+                        onChange={handleChangeInputVal}
+                    />
                 </div>
             </div>
-            <div className='add-character-button'>
+            <div className='add-character-button' onClick={()=>addCharacter(inputValue)}>
                 다음
             </div>
         </div>
