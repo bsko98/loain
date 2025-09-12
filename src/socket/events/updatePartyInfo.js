d4218e6a7df7f9d5e1e474ecbfe889191f01d18e
bsko98 <bsko98@naver.com>
2025-09-12 21:18:47 +0900

    feat: 파티 정보 수정 로직 추가, 내 파티 페이지에서 파티원 정보 보이게 수정,

--- src/socket/events/updatePartyInfo.js
+++ src/socket/events/updatePartyInfo.js
@@ -1,13 +1,16 @@
+import { PartyEntityMapper } from "../../mappers/entityMappers/partyEntityMapper";
 
 export const updatePartyInfoEventHandler = (states) => {
-    return {
-        name: "",
-        handle: (data) => {
-            if(data.status === "error") {
-                console.log("Error");
-                return;
-            }
-            console.log(`!`)
-        }
-    }
-}
+  return {
+    name: "updatePartyInfo",
+    handle: (data) => {
+      if (data.status === "error") {
+        console.log("Error");
+        return;
+      }
+      states.myParty = PartyEntityMapper.toInternal(data.partyData);
+      states.setMyParty({ ...states.myParty });
+      console.log(`파티 정보 수정 성공했습니다.`);
+    },
+  };
+};
